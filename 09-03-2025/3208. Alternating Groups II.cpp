205a8b8e4dfede776b5bf53290fad93ae2d908c2
B-asmala <basmalabayoumi@gmail.com>
2025-03-10 09:50:38 +0200

    added both solutions

--- 09-03-2025/3208. Alternating Groups II.cpp
+++ 09-03-2025/3208. Alternating Groups II.cpp
@@ -1,3 +1,44 @@
+//Solution 1
+// class Solution {
+// public:
+//     int numberOfAlternatingGroups(vector<int>& temp, int& k) {
+
+//         for (int i = 0; i < k - 1; i++)
+//             temp.push_back(temp[i]);
+//         int n = temp.size();
+//         int ans = 0;
+//         int i = 0;
+//         bool pass = false;
+//         while (i < n) {
+//             if (pass) {
+//                 if (temp[i] == temp[i - 1]) {
+//                     pass = false;
+//                 } else {
+//                     ans++;
+//                     i++;
+//                 }
+//             } else {
+//                 if (i > n - k)
+//                     break;
+//                 int j = i;
+//                 int last = -1;
+//                 while (j < i + k && temp[j] != last) {
+//                     last = temp[j];
+//                     j++;
+//                 }
+//                 if (j == i + k) {
+//                     ans++;
+//                     pass = true;
+//                 }
+//                 i = j;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+//Solution 2
 class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& colors, int k) {
